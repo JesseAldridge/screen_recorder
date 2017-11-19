f0c2e998e7e5693b5a39251f0b683483dad87ef3
Jesse Aldridge <JesseAldridge@gmail.com>
2017-11-19 00:40:40 -0800

    just run in while loop

--- monitor_screen_rec.sh
+++ monitor_screen_rec.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-until ~/Dropbox/screen_recorder/_2_take_screenshots.py; do
-    echo "screen_recorder crashed with exit code $?.  Respawning.." >> ~/screenshots.err >&2
-    sleep 10
-done
+while [ 1 ]; do
+  ~/Dropbox/screen_recorder/_2_take_screenshots.py
+  sleep 10;
+done;
