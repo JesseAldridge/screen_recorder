4d68e2644af3d5664f6b228a1dc5da8aa0cf3c16
Jesse Aldridge <jessealdridge@gmail.com>
2018-10-20 23:08:22 -0700

    only save seven days

--- download.py
+++ download.py
@@ -13,7 +13,7 @@ SECRET_KEY = lines[2].split(' = ')[1]
 USERNAME = config.config_dict.get('username')
 
 bucket_name = 'jca-screenshots-{}-normal'.format(USERNAME)
-url = 'http://{}.s3.amazonaws.com?prefix=2018-01-01'.format(bucket_name)
+url = 'http://{}.s3.amazonaws.com?prefix=2018-01-19'.format(bucket_name)
 
 resp = requests.get(url, auth=S3Auth(ACCESS_KEY, SECRET_KEY))
 print 'status:', resp.status_code
